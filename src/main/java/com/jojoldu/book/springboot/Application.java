60d056895be824a056d436e203f864b6d0eb4a08
chanhl22 <chanhlee@student.42seoul.kr>
2021-06-30 23:52:29 +0900

    Update build.gradle & Application Class

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -1,7 +1,11 @@
 package com.jojoldu.book.springboot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        System.out.println("test!");
+        SpringApplication.run(Application.class, args);
     }
 }
