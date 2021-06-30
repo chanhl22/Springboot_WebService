48edd9bf9f6b05825bc7f43a5d892ff67e6197eb
chanhl22 <chanhlee@student.42seoul.kr>
2021-06-30 21:27:21 +0900

    test

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,6 +2,6 @@ package com.jojoldu.book.springboot;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test!");
     }
 }
