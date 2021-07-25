e1bcf7e315312cfbfd898eb15b68562ceee87d53
chanhl22 <chanhlee@student.42seoul.kr>
2021-07-25 16:54:43 +0900

    람다

--- src/main/java/com/jojoldu/book/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -15,7 +15,11 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Function;
 
 @RequiredArgsConstructor
 @Service
@@ -35,6 +39,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
 
         User user = saveOrUpdate(attributes);
+//        User user = this.saveOrUpdate(attributes);
         httpSession.setAttribute("user", new SessionUser(user));
 
         return new DefaultOAuth2User(
@@ -43,11 +48,47 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
                 attributes.getNameAttributeKey());
     }
 
+//    public<U> Optional<U> map(Function<? super T, ? extends U> mapper) {
+//        Objects.requireNonNull(mapper);
+//        if (!isPresent())
+//            return empty();
+//        else {
+//            return Optional.ofNullable(mapper.apply(value));
+//        }
+//    }
 
     private User saveOrUpdate(OAuthAttributes attributes) {
         User user = userRepository.findByEmail(attributes.getEmail())
                 .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
                 .orElse(attributes.toEntity());
+
+//        User user = userRepository.findByEmail(attributes.getEmail()).get();
+//        if (user == null) {
+//            user = attributes.toEntity();
+//        } else {
+//            user.update(attributes.getName(), attributes.getPicture());
+//        }
+
+//                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+//                .map(inputuser -> inputuser.update(attributes.getName(), attributes.getPicture()))
+
+//                .map(inputuser -> {
+//            User outputuser = inputuser.update(attributes.getName(), attributes.getPicture());
+//            return outputuser;
+//                })
+
+//        Optional<User> optional = Optional.ofNullable(new User());
+//        optional
+//                .map(
+//                        new Function<User, User>() {
+//                            @Override
+//                            public User apply(User inputuser) {
+//                                User outputuser = inputuser.update(attributes.getName(), attributes.getPicture());
+//                                return outputuser;
+//                            }
+//                        }
+//                )
+//                .orElse(attributes.toEntity());
 
         return userRepository.save(user);
     }
